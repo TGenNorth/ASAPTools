f4962a1f15795cc613e15255d9d10b1c23fab3d5
W. Tanner Porter <tporter@tgen.org>
2024-08-01 16:14:30 -0700

    ASAP.get.smor.discards has parallel compute

--- R/ASAP_get_SMOR_discards.R
+++ R/ASAP_get_SMOR_discards.R
@@ -1,46 +1,48 @@
 #' Process ASAP DF for SMOR discards
 #' @description
 #' Function will use a user specified dataframe from read.ASAP function and parse the SMOR discards data into a df that can be easily plotted.
-#' @param read.ASAP imported df
+#' @param read.ASAP.df imported df
+#' @param num_cores number of cores to use for parallel processing
 #' @return An object with data from the read.ASAP dataframe.
-#' @export ASAP_get_smor_discards
+#' @export ASAP.get.smor.discards
 #' @importFrom dplyr bind_rows
 #' @importFrom stringr str_replace
+#' @importFrom foreach foreach %dopar%
+#' @importFrom doParallel registerDoParallel
 
-ASAP_get_smor_discards <- function(read.ASAP.df){
+ASAP.get.smor.discards <- function(read.ASAP.df, num_cores = 1) {
+  library(dplyr)
+  library(stringr)
+  library(foreach)
+  library(doParallel)
 
-  smor_discards_out <- data.frame()
+  # Register parallel backend
+  cl <- makeCluster(num_cores)
+  registerDoParallel(cl)
 
-  for (i in 1:nrow(read.ASAP.df)) {
-
-    run = read.ASAP.df$run[[i]]
-
-    name = read.ASAP.df$name[[i]]
-
-    assay_name = read.ASAP.df$assay_name[[i]]
-
+  smor_discards_out <- foreach(i = 1:nrow(read.ASAP.df), .combine = bind_rows) %dopar% {
+    run <- read.ASAP.df$run[[i]]
+    name <- read.ASAP.df$name[[i]]
+    assay_name <- read.ASAP.df$assay_name[[i]]
     avg_breadth <- read.ASAP.df$breadth[[i]]
-
     smor_discards <- read.ASAP.df$smor_discards[[i]]
 
-    # discards <- stringr::str_replace_all(discards, "\"", "")
-
+    # Remove any extra quotes and split by comma
     smor_discards <- unlist(strsplit(smor_discards, ","))
-
     smor_discards <- as.numeric(smor_discards)
+    position <- seq(1, length(smor_discards))
 
-    position <- seq(1:length(smor_discards))
-
-    temp <- as.data.frame(cbind(run, name, assay_name, position, smor_discards, avg_breadth))
-
-    smor_discards_out <- dplyr::bind_rows(smor_discards_out, temp)
+    temp <- data.frame(run, name, assay_name, position, smor_discards, avg_breadth)
 
     print(paste("Processing complete for:", name))
+    return(temp)
   }
+
+  # Stop the parallel cluster
+  stopCluster(cl)
 
   smor_discards_out$position <- as.numeric(as.character(smor_discards_out$position))
   smor_discards_out$smor_discards <- as.numeric(as.character(smor_discards_out$smor_discards))
-
   smor_discards_out$avg_breadth <- as.numeric(as.character(smor_discards_out$avg_breadth))
 
   return(smor_discards_out)
